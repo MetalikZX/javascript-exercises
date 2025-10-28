3d1500ad776c991a3aaafaee826e6630004adcf9
Metalik <1781573+MetalikZX@users.noreply.github.com>
2025-10-28 21:11:36 +0300

    Solved 08_REMOVEFROMARRAY

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = (array, ...remValue) => array.filter(item => !remValue.includes(item));
+
+// {
+    // for (let i = array.length - 1; i >= 0; i--) {
+    // const indexValue = array.indexOf(remValue);
+    // if (indexValue !== -1) {
+    //     array.splice(indexValue, 1);
+    // }
+    // }
 
 // Do not edit below this line
 module.exports = removeFromArray;
