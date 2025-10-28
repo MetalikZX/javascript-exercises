6520e3db476c8a04cffe4b40760a3564f55636fc
Metalik <1781573+MetalikZX@users.noreply.github.com>
2025-10-28 23:54:18 +0300

    Solved 11_Tempconversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const conCel = ((temp - 32) * 5/9);
+  const roundStr = conCel.toFixed(1);
+  const finalNumber = parseFloat(roundStr);
+  return finalNumber;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const conCel = ((9/5 * temp) + 32);
+  const roundStr = conCel.toFixed(1);
+  const finalNumber = parseFloat(roundStr);
+  return finalNumber;
 };
 
 // Do not edit below this line
